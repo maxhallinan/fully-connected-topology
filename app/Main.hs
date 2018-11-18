2dc377400780ab4bd6e8d5698433694faa63515f
Max Hallinan <maxhallinan@riseup.net>
2018-11-18 20:31:37 +0100

    Parse address arguments

--- app/Main.hs
+++ app/Main.hs
@@ -1,39 +1,51 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
-import Options.Applicative
+import Control.Applicative ((<|>), some)
+import Data.Void (Void)
 import Data.Semigroup ((<>))
+import Lib
+import qualified Text.Megaparsec as Mega
+import qualified Text.Megaparsec.Char as Mega
+import Options.Applicative ((<**>)) 
+import qualified Options.Applicative as Opt
+import qualified System.Network as Net
 
 newtype Host = Host String deriving (Eq, Show)
 newtype Port = Port String deriving (Eq, Show)
 data Address = Address Host Port deriving (Eq, Show)
 data Args = Args Address [Address] deriving (Eq, Show)
 
-argsParser :: Parser Args
+type Parsec = Mega.Parsec Void String
+
+addressParser :: Parsec Address
+addressParser = do
+  host <- some (Mega.alphaNumChar <|> Mega.char '.')
+  Mega.char ':'
+  port <- some Mega.digitChar
+  return $ Address (Host host) (Port port)
+
+peersParser :: Parsec [Address]
+peersParser = addressParser `Mega.sepBy` Mega.char ','
+
+argsParser :: Opt.Parser Args
 argsParser = Args <$> ownAddressParser <*> peerAddressesParser
   where 
-    ownAddressParser = fmap toAddress $ strArgument helpText
-      where helpText = metavar "OWN_ADDRESS" <> help "client's address" 
+    ownAddressParser = Opt.argument argReader helpText
+      where 
+        argReader = Opt.maybeReader (Mega.parseMaybe addressParser)
+        helpText  = Opt.metavar "OWN_ADDRESS" <> Opt.help "client's address" 
 
-    peerAddressesParser = fmap toAddressList $ strArgument helpText
-      where helpText = metavar "PEER_ADDRESSES" <> help "peer addresses" 
-
-    toAddressList :: String -> [Address] 
-    toAddressList string =
-      -- placeholder
-      [toAddress ""]
-
-    toAddress :: String -> Address
-    toAddress string =
-      -- placeholder
-      Address (Host "") (Port "")
+    peerAddressesParser = Opt.argument argReader helpText
+      where 
+        argReader = Opt.maybeReader (Mega.parseMaybe peersParser)
+        helpText  = Opt.metavar "PEER_ADDRESSES" <> Opt.help "peer addresses" 
 
 runProgramWithArgs :: Args -> IO ()
 runProgramWithArgs (Args ownAddress peerAddresses) = do
-  -- placeholder
   putStrLn $ show ownAddress
   putStrLn $ show peerAddresses
 
 main :: IO ()
-main = runProgramWithArgs =<< execParser opts 
-  where opts = info (argsParser <**> helper) fullDesc
+main = runProgramWithArgs =<< Opt.execParser opts 
+  where opts = Opt.info (argsParser <**> Opt.helper) Opt.fullDesc
