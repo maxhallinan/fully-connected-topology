610254107466de6db8667e50d53b2263f9eb2030
Max Hallinan <maxhallinan@riseup.net>
2018-12-04 17:34:53 +0100

    Refactor connect

--- src/Network/FullyConnected.hs
+++ src/Network/FullyConnected.hs
@@ -29,6 +29,11 @@ import qualified System.IO.Error as IO.Error
 import qualified Text.Megaparsec as Mega
 import qualified Text.Megaparsec.Char as Mega
 
+type Connections = Concurrent.MVar [Net.Socket]
+
+initConnections :: IO Connections
+initConnections = Concurrent.newMVar []
+
 data Address = Address Host Port deriving (Eq)
 
 instance Show Address where
@@ -56,10 +61,20 @@ addressParser = do
   portString <- some Mega.digitChar
   return $ Address (Host hostString) (Port portString)
 
-type Connections = Concurrent.MVar [Net.Socket]
+broadcast :: Connections -> String -> IO ()
+broadcast connections message = do
+  sockets <- Concurrent.readMVar connections
+  fold <$> traverse (sendMessage message) sockets
+  where
+    send :: String -> Net.Socket -> IO ()
+    send message socket = void $ forkIO $ do
+      sendMessage message socket
 
-initConnections :: IO Connections
-initConnections = Concurrent.newMVar []
+connect :: [Address] -> IO Connections
+connect addresses = do
+  connections <- initConnections
+  fold <$> traverse (addPeer connections) addresses
+  return connections
 
 type MessageHandler = String -> IO ()
 
@@ -69,15 +84,6 @@ listen handleMessage ownAddress = Net.withSocketsDo $ do
     (startServer ownAddress)
     closeSocket
     (handleConnections handleMessage)
-
-broadcast :: Connections -> String -> IO ()
-broadcast connections message = do
-  sockets <- Concurrent.readMVar connections
-  fold <$> traverse (sendMessage message) sockets
-  where
-    send :: String -> Net.Socket -> IO ()
-    send message socket = void $ forkIO $ do
-      sendMessage message socket
 
 sendMessage :: String -> Net.Socket -> IO ()
 sendMessage message peerSocket = do
@@ -97,7 +103,7 @@ sendMessage message peerSocket = do
     handleError error = do
       -- TODO: What is the best approach to error handling?
       -- Retry the send? Close the socket? Close, reconnect, and send?
-      putStrLn $ "Error sending the message: " ++ (show error)
+      putStrLn $ "Error sending message: " ++ (show error)
 
 createSocket :: Net.AddrInfo -> IO Net.Socket
 createSocket addrInfo =
@@ -117,108 +123,91 @@ resolveAddress (Address host port) = do
   let hints = Net.defaultHints { Net.addrFlags = [Net.AI_PASSIVE]
                                , Net.addrSocketType = Net.Stream
                                }
-  (addressInfo : _) <- Net.getAddrInfo
+  (addrInfo : _) <- Net.getAddrInfo
                         (Just hints)
                         (Just $ coerce host)
                         (Just $ coerce port)
-  return addressInfo
+  return addrInfo
 
 listenAtAddress :: Net.AddrInfo -> IO Net.Socket
-listenAtAddress addressInfo = do
+listenAtAddress addrInfo = do
   socket <- Net.socket
-              (Net.addrFamily addressInfo)
-              (Net.addrSocketType addressInfo)
-              (Net.addrProtocol addressInfo)
-  Net.bind socket (Net.addrAddress addressInfo)
+              (Net.addrFamily addrInfo)
+              (Net.addrSocketType addrInfo)
+              (Net.addrProtocol addrInfo)
+  Net.bind socket (Net.addrAddress addrInfo)
   -- set the max queue size for connection requests to 5
   Net.listen socket 5
-  putStrLn $ "Now listening on " ++ (showAddrInfo addressInfo)
+  putStrLn $ "Listening on " ++ (showAddrInfo addrInfo)
   return socket
 
 handleConnections :: MessageHandler -> Net.Socket -> IO ()
 handleConnections handleMessage ownSocket = forever $ do
   (peerSocket, peerAddress) <- Net.accept ownSocket
   forkFinally
-    (receiveMessage handleMessage (peerSocket, peerAddress))
+    (receiveMessage handleMessage peerSocket)
     (\_ -> Net.close peerSocket)
-  putStrLn $ "Now receiving messages from " ++ show peerAddress
+  putStrLn $ "Receiving messages from " ++ show peerAddress
 
-receiveMessage :: MessageHandler -> (Net.Socket, Net.SockAddr) -> IO ()
-receiveMessage handleMessage (peerSocket, peerAddress) = do
+receiveMessage :: MessageHandler -> Net.Socket -> IO ()
+receiveMessage handleMessage peerSocket = do
   message <- NetByte.recv peerSocket 1024
   if ByteChar.length message == 0
     then do
       closeSocket peerSocket
     else do
       handleMessage $ ByteChar.unpack message
-      receiveMessage handleMessage (peerSocket, peerAddress)
+      receiveMessage handleMessage peerSocket
 
-connect :: [Address] -> IO Connections
-connect addresses = do
-  connections <- initConnections
-  fold <$> traverse (connect connections) addresses
-  return connections
-  where
-    connect :: Connections -> Address -> IO ()
-    connect connections address = void $ forkIO $ do
+addPeer :: Connections -> Address -> IO ()
+addPeer connections address = void $ forkIO $ do
+  void $ Exception.bracketOnError peerSocket retryConnect connect
+  where 
+    peerSocket :: IO (Net.AddrInfo, Net.Socket)
+    peerSocket = do 
       addrInfo <- resolveAddress address
-      Exception.catch (connection addrInfo) (retryConnect addrInfo)
-      where
-        connection :: Net.AddrInfo -> IO ()
-        connection addrInfo = connectToPeer handleConnection addrInfo
+      peerSocket <- createSocket addrInfo
+      return (addrInfo, peerSocket)
 
-        handleConnection :: Net.Socket -> IO ()
-        handleConnection c = do
-          Concurrent.modifyMVar connections (updateConnections c)
-          void $ forkIO $ cleanup connections c
+    connect :: (Net.AddrInfo, Net.Socket) -> IO Net.Socket
+    connect (addrInfo, peerSocket) = do
+      putStrLn $ "Connecting to peer: " ++ (showAddrInfo addrInfo)
+      Net.connect peerSocket $ Net.addrAddress addrInfo
+      Concurrent.modifyMVar connections (add peerSocket)
+      forkIO $ listenForRemoteClose connections addrInfo peerSocket
+      putStrLn $ "Connected to peer: " ++ (showAddrInfo addrInfo)
+      return peerSocket
 
-        updateConnections :: Net.Socket -> [Net.Socket] -> IO ([Net.Socket], ())
-        updateConnections c cs = return (c : cs, ())
+    add :: Net.Socket -> [Net.Socket] -> IO ([Net.Socket], ())
+    add peerSocket connections = return (peerSocket : connections, ())
 
-        retryConnect :: Net.AddrInfo -> IO.Error.IOError -> IO ()
-        retryConnect addrInfo _ = do
-          putStrLn $ "Retrying connection to peer: " ++ (showAddrInfo addrInfo)
-          void $ Concurrent.threadDelay 1000000
-          void $ connect connections address
+    retryConnect :: (Net.AddrInfo, Net.Socket) -> IO ()
+    retryConnect (addrInfo, peerSocket) = do
+      putStrLn $ "Failed connection to peer: " ++ (showAddrInfo addrInfo)
+      closeSocket peerSocket
+      void $ Concurrent.threadDelay 1000000
+      putStrLn $ "Retrying connection to peer: " ++ (showAddrInfo addrInfo)
+      addPeer connections address
 
-        -- Remove socket from Connections when the remote socket closes
-        cleanup :: Connections -> Net.Socket -> IO ()
-        cleanup connections peerSocket = do
-          message <- NetByte.recv peerSocket 1024
-          if ByteChar.length message == 0
-            then do
-              removePeer connections peerSocket
-              putStrLn "Closed connection"
-            else do
-              cleanup connections peerSocket
+    listenForRemoteClose :: Connections -> Net.AddrInfo -> Net.Socket -> IO ()
+    listenForRemoteClose connections addrInfo peerSocket = do
+      message <- NetByte.recv peerSocket 1024
+      if ByteChar.length message == 0
+        then do
+          -- Remove socket from Connections when the remote socket closes
+          removePeer connections peerSocket
+          putStrLn $ "Remotely closed connection to peer: " ++ (showAddrInfo addrInfo)
+        else do
+          listenForRemoteClose connections addrInfo peerSocket
 
+-- TODO: Store connected sockets keyed by Address so that the peer is removed by
+-- address instead of socket.
+-- This would make it suitable for the module's public API.
 removePeer :: Connections -> Net.Socket -> IO ()
 removePeer connections peerSocket = do
   closeSocket peerSocket
   Concurrent.modifyMVar connections remove
   where remove cs = return (filter (/= peerSocket) cs, ())
-
-connectToPeer :: (Net.Socket -> IO ()) -> Net.AddrInfo -> IO ()
-connectToPeer handleConnection addrInfo = do
-  Exception.bracketOnError socket close connect
-  where
-        peerIp = showAddrInfo addrInfo
-
-        socket :: IO Net.Socket
-        socket = Net.socket
-          (Net.addrFamily addrInfo)
-          (Net.addrSocketType addrInfo)
-          (Net.addrProtocol addrInfo)
-
-        connect :: Net.Socket -> IO ()
-        connect peerSocket = do
-          putStrLn $ "Connecting to peer: " ++ peerIp
-          Net.connect peerSocket $ Net.addrAddress addrInfo
-          handleConnection peerSocket
-          putStrLn $ "Connected to peer: " ++ peerIp
-
-        close :: Net.Socket -> IO ()
-        close peerSocket = closeSocket peerSocket
 
 showAddrInfo :: Net.AddrInfo -> String
 showAddrInfo = show . Net.addrAddress
